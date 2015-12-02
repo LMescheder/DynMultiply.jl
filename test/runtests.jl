44f183692809d3f48a52ead6eecbb502548c7825
Lars Mescheder <larsmescheder@gmx.net>
2015-12-02 18:14:58 +0100

    Set up tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,21 @@
 using DynMultiply
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# first test set
+A = rand(1000, 1000)
+B = rand(1000, 1000)
+v = rand(1000)
+
+@test dynmultiply(A, B, v) ≈ A * B * v
+@test dynmultiplystr(A, B, v) == "(A1*(A2*A3))"
+@test dynmultiply_plan(A, B, v).data == [0,0,0,1,1,1]
+
+# second test set
+A = rand(1000, 1)
+B = rand(1, 1000)
+C = rand(1000, 50)
+D = rand(50, 1)
+
+@test dynmultiply(A, B, C, D) ≈ A * B * C * D
+@test dynmultiplystr(A, B, C, D) == "(A1*((A2*A3)*A4))"
+@test dynmultiply_plan(A, B, C, D).data == [0,0,0,0,1,1,1,1,2,1]
